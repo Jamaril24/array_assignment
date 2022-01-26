91d502167638cbe250a4301a1713fec0d7c76e51
Jamaril24 <jamarlopez24@g.crookcountyschools.org>
2022-01-26 10:09:19 -0800

    added

---
+++ script.js
@@ -0,0 +1,15 @@
+'use script'
+ 
+document.write("<h1>Arrays Assignment</h1>")
+
+const icecream = ["Chocolate", "Vanilla", "Strawbery", "Rainbow", "Mint" ];
+
+console.log(icecream[1]);
+
+icecream.push("Strawberry Cheesecake");
+
+const apps = ["Snapchat", "Instagram", "Facebook", "Spotify", "Tiktok"];
+
+console.log(apps[2]);
+
+apps.shift(Tiktok);
